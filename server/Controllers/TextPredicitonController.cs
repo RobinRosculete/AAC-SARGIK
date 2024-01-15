d44778e07a91ba1a0e18ff864b637bbc1d5f3f0e
RobinRosculete <robin.rosculete.860@my.csun.edu>
2024-01-15 12:05:00 -0800

    Added connection to Chat GPT API

--- server/Controllers/TextPredicitonController.cs
+++ server/Controllers/TextPredicitonController.cs
@@ -1,46 +1,41 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OpenAI_API;
+using OpenAI_API.Models;
+
 
 namespace server.Controllers;
 
     [Route("api/[controller]")]
     [ApiController]
-    public class TextPredicitonController : ControllerBase
+    public class TextPredictionController : ControllerBase
     {
-        // GET: api/TextPrediciton
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly IConfiguration _configuration;
+
+        public TextPredictionController(IConfiguration configuration)
         {
-            return new string[] { "value1", "value2" };
+            _configuration = configuration;
         }
 
-        // GET: api/TextPrediciton/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+     // GET: api/TextPrediciton
+    [HttpGet]
+    public async Task<IActionResult> UseChatGPT(string query)
+    {
+        // Base Prompt, not complete
+        string basePrompt = "Only continue and try to predict what the user wanted to say. Only use one centence. " +
+            "Please only continue the frazes with that a person with impeard speach would say to comunicate with people." +
+            " General Expressions that people will use in there day to day life.";
+        string GPT_API_KEY = _configuration.GetConnectionString("GPT_API_KEY");
 
-        // POST: api/TextPrediciton
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+        OpenAIAPI api = new OpenAIAPI(GPT_API_KEY);
+        var chat = api.Chat.CreateConversation();
+        chat.AppendUserInput(query + basePrompt);
+        chat.Model = Model.GPT4_Turbo;
 
-        // PUT: api/TextPrediciton/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        string response = await chat.GetResponseFromChatbotAsync();
+        return Ok(response);
 
-        // DELETE: api/TextPrediciton/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
+
+
+}
 
