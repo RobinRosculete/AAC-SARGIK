8a4e423b462cef6bc33162e66416c8afef13c16e
RobinRosculete <robin.rosculete.860@my.csun.edu>
2024-01-15 16:05:05 -0800

    Added Keyboard and Styling

--- client/src/app/components/keyboard/keyboard.component.ts
+++ client/src/app/components/keyboard/keyboard.component.ts
@@ -1,10 +1,117 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewEncapsulation } from '@angular/core';
+import Keyboard from 'simple-keyboard';
+// KeyboardComponent
 @Component({
   selector: 'app-keyboard',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './keyboard.component.html',
-  styleUrls: ['./keyboard.component.css']
+  styleUrls: ['./keyboard.component.css'],
 })
 export class KeyboardComponent {
+  value = '';
+  keyboard!: Keyboard;
 
+  ngAfterViewInit() {
+    this.keyboard = new Keyboard({
+      onChange: (input) => this.onChange(input),
+      onKeyPress: (button) => this.onKeyPress(button),
+      theme: 'hg-theme-default hg-theme-ios',
+      layout: {
+        default: [
+          'q w e r t y u i o p {bksp}',
+          'a s d f g h j k l {enter}',
+          '{shift} z x c v b n m , . {shift}',
+          '{alt} {smileys} {space} {altright} {downkeyboard}',
+        ],
+        shift: [
+          'Q W E R T Y U I O P {bksp}',
+          'A S D F G H J K L {enter}',
+          '{shiftactivated} Z X C V B N M , . {shiftactivated}',
+          '{alt} {smileys} {space} {altright} {downkeyboard}',
+        ],
+        alt: [
+          '1 2 3 4 5 6 7 8 9 0 {bksp}',
+          `@ # $ & * ( ) ' " {enter}`,
+          '{shift} % - + = / ; : ! ? {shift}',
+          '{default} {smileys} {space} {back} {downkeyboard}',
+        ],
+        smileys: [
+          '😀 😊 😅 😂 🙂 😉 😍 😛 😠 😎 {bksp}',
+          `😏 😬 😭 😓 😱 😪 😬 😴 😯 {enter}`,
+          '😐 😇 🤣 😘 😚 😆 😡 😥 😓 🙄 {shift}',
+          '{default} {smileys} {space} {altright} {downkeyboard}',
+        ],
+      },
+      display: {
+        '{alt}': '.?123',
+        '{smileys}': '\uD83D\uDE03',
+        '{shift}': '⇧',
+        '{shiftactivated}': '⇧',
+        '{enter}': 'return',
+        '{bksp}': '⌫',
+        '{altright}': '.?123',
+        '{downkeyboard}': '🞃',
+        '{space}': ' ',
+        '{default}': 'ABC',
+        '{back}': '⇦',
+      },
+    });
+  }
+
+  onChange = (input: string) => {
+    this.value = input;
+    console.log('Input changed', input);
+  };
+
+  onKeyPress = (button: string) => {
+    console.log('Button pressed', button);
+
+    /**
+     * Handle toggles
+     */
+    if (button.includes('{') && button.includes('}')) {
+      this.handleLayoutChange(button);
+    }
+  };
+  handleLayoutChange = (button: string) => {
+    let currentLayout = this.keyboard.options.layoutName;
+    let layoutName;
+
+    switch (button) {
+      case '{shift}':
+      case '{shiftactivated}':
+      case '{default}':
+        layoutName = currentLayout === 'default' ? 'shift' : 'default';
+        break;
+
+      case '{alt}':
+      case '{altright}':
+        layoutName = currentLayout === 'alt' ? 'default' : 'alt';
+        break;
+
+      case '{smileys}':
+        layoutName = currentLayout === 'smileys' ? 'default' : 'smileys';
+        break;
+
+      default:
+        break;
+    }
+    if (layoutName) {
+      this.keyboard.setOptions({
+        layoutName: layoutName,
+      });
+    }
+  };
+  onInputChange = (event: any) => {
+    this.keyboard.setInput(event.target.value);
+  };
+
+  handleShift = () => {
+    let currentLayout = this.keyboard.options.layoutName;
+    let shiftToggle = currentLayout === 'default' ? 'shift' : 'default';
+
+    this.keyboard.setOptions({
+      layoutName: shiftToggle,
+    });
+  };
 }
