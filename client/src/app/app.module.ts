99834044a2bab89a9dbec665cef45b71c16e9efc
Armon Lee <armonlee99@gmail.com>
2024-02-07 11:58:55 -0800

    added feature component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { KeyboardComponent } from './components/keyboard/keyboard.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FeaturesComponent } from './components/features/features.component';
 
 @NgModule({
-  declarations: [AppComponent, NavMenuComponent, KeyboardComponent],
+  declarations: [AppComponent, NavMenuComponent, KeyboardComponent, FeaturesComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
