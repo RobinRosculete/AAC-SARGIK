dca151680527545de4acaa747eb7e04aa2df756a
RobinRosculete <robin.rosculete.860@my.csun.edu>
2024-02-18 20:13:37 -0800

    Removed Azure Ad Part

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,17 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavmenuComponent } from './components/nav-menu/nav-menu.component';
 import { KeyboardComponent } from './components/keyboard/keyboard.component';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { GalleryComponent } from './components/gallery/gallery.component';
-
-import {
-  MsalInterceptor,
-  MsalModule,
-  MsalGuard,
-  MsalRedirectComponent,
-} from '@azure/msal-angular';
-import { InteractionType, PublicClientApplication } from '@azure/msal-browser';
-import { environment } from 'src/environments/environment.development';
 
 @NgModule({
   declarations: [
@@ -24,42 +15,8 @@ import { environment } from 'src/environments/environment.development';
     KeyboardComponent,
     GalleryComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    MsalModule.forRoot(
-      new PublicClientApplication({
-        auth: {
-          clientId: environment.clientId,
-          authority: environment.authorities.signUpSignIn.authority,
-          redirectUri: environment.redirectUri,
-          knownAuthorities: [environment.authorityDomain],
-        },
-        cache: {
-          cacheLocation: 'localStorage',
-          storeAuthStateInCookie: false,
-        },
-      }),
-      {
-        interactionType: InteractionType.Redirect,
-        authRequest: {
-          scopes: ['openid'],
-        },
-      },
-      {
-        interactionType: InteractionType.Redirect,
-        protectedResourceMap: new Map([
-          [environment.endpoint, environment.endpointScopes],
-        ]),
-      }
-    ),
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: MsalInterceptor, multi: true },
-    MsalGuard,
-  ],
-  bootstrap: [AppComponent, MsalRedirectComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
