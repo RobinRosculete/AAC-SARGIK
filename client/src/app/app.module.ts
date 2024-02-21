5d7358d8bb6d4d32065e6d551c994313bc741dac
RobinRosculete <robin.rosculete.860@my.csun.edu>
2024-02-21 12:45:54 -0800

    Fixed Bugs

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,12 +7,22 @@ import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { KeyboardComponent } from './components/keyboard/keyboard.component';
 import { HttpClientModule } from '@angular/common/http';
 import { VsdComponent } from './components/vsd/vsd.component';
-import { MatDialogModule} from '@angular/material/dialog';
-
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
-  declarations: [AppComponent, NavMenuComponent, KeyboardComponent, VsdComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, MatDialogModule],
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    KeyboardComponent,
+    VsdComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    MatDialogModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
