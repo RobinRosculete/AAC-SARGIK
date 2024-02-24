3ffa651b630ce68b518174809486255ecd9ff7af
RobinRosculete <robin.rosculete.860@my.csun.edu>
2024-02-24 12:22:28 -0800

    Merged Wiht Kaijing Branch

--- client/src/app/components/gallery/gallery.component.ts
+++ client/src/app/components/gallery/gallery.component.ts
@@ -1,21 +1,16 @@
 import { Component } from '@angular/core';
-
-
-
-import { IonModal } from '@ionic/angular';
-import { OverlayEventDetail } from '@ionic/core/components';
-import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css'],
 })
 export class GalleryComponent {
-  //title = 'Gallery';
-  //constructor(private imageService: ImageService){}  
-  //image: number = 1;//declear for image number
-  constructor(){
-    }
-
- 
+  images: number[] = [1, 2, 3, 4, 5];
+  constructor() {}
+  ngOnInit(): void {
+    setTimeout(() => {
+      // Load images after a small delay to allow container to calculate its height
+      this.images = [1, 2, 3, 4, 5];
+    }, 100);
+  }
 }
