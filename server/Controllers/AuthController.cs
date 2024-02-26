6ce1a691d11c2fd5549254ec886b24e71dae90a0
RobinRosculete <robin.rosculete.860@my.csun.edu>
2024-02-25 21:37:32 -0800

    Added Auth Srevice to server

--- server/Controllers/AuthController.cs
+++ server/Controllers/AuthController.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace server.Controllers
 {
     [Route("api/[controller]")]
-    public class UserController : Controller
+    public class AuthController : Controller
     {
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
         {
-            return new string[] { "value1", "value2" };
         }
+
+        
     }
 }
 
--- server/Program.cs
+++ server/Program.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.Identity.Web;
-using Google.Apis.Auth;
+
 
 var builder = WebApplication.CreateBuilder(args);
-
+var configuration = builder.Configuration;
 // Add services to the container.
-builder.Services.AddAuthorization(); 
+builder.Services.AddAuthorization();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
