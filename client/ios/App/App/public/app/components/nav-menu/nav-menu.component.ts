4073c564d252759068a787aa0d81d88d98f9eda7
RobinRosculete <robin.rosculete.860@my.csun.edu>
2024-02-25 21:36:34 -0800

    Added Splash Screen Adn Icon

--- client/ios/App/App/public/app/components/nav-menu/nav-menu.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-navmenu',
-  templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css'],
-})
-export class NavmenuComponent {}
