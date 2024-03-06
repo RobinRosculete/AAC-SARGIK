6db8ac24eb7977b33adf76b3848729483d880625
karizheng <127488346+karizheng@users.noreply.github.com>
2024-03-06 12:11:10 -0800

    gallery

--- client/src/app/components/gallery/gallery.component.ts
+++ client/src/app/components/gallery/gallery.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 
 import { IonModal } from '@ionic/angular';
@@ -11,11 +11,22 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./gallery.component.css'],
 })
 export class GalleryComponent {
+  //captions:{[key:number]:string}={};
+  images = [
+    {number:1, caption:'peacock'},
+    {number:2, caption:'red flower'},
+    {number:3, caption:'sea'},
+    {number:4, caption:'white flower'},
+    {number:5, caption:'flower'}
+  ];
   //title = 'Gallery';
   //constructor(private imageService: ImageService){}  
   //image: number = 1;//declear for image number
   constructor(){
+    //for(let i=1;i<=5;i++){
+      //this.captions[i]='';
+    //}
     }
 
  
-}
+  }
